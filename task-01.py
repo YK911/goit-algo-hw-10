ef409413fe71481d6659812c346a9c766bf2fcd2
yaroslav.kosytsia <jaroslavkositsa@gmail.com>
2024-02-07 17:00:47 +0200

    Update task-01.py

--- task-01.py
+++ task-01.py
@@ -1,27 +1,32 @@
 import pulp
 
 
-"""
-| Максимізація виробництва напоїв
-"""
-model = pulp.LpProblem("Drinks_production", pulp.LpMaximize)
+# Максимізація виробництва напоїв
+def drinks_production():
+    model = pulp.LpProblem("Drinks_production", pulp.LpMaximize)
 
-# Визначення змінних
-A = pulp.LpVariable("A", lowBound=0, cat="Integer")  # Кількість Лимонаду
-B = pulp.LpVariable("B", lowBound=0, cat="Integer")  # Кількість Фруктового соку
+    # Визначення змінних
+    A = pulp.LpVariable("A", lowBound=0, cat="Integer")  # Кількість Лимонаду
+    B = pulp.LpVariable("B", lowBound=0, cat="Integer")  # Кількість Фруктового соку
 
-# Максимізація прибутку
-model += A + B, "Profit"
+    # Максимізація прибутку
+    model += A + B, "Profit"
 
-# Система обмежень
-model += 2 * A + 1 * B <= 100  # Обмеження для води
-model += 1 * A <= 50  # Обмеження для цукру
-model += 1 * A <= 30  # Обмеження для лимонного соку
-model += 2 * B <= 40  # Обмеження для фруктового пюре
+    # Система обмежень
+    model += 2 * A + 1 * B <= 100  # Обмеження для води
+    model += 1 * A <= 50  # Обмеження для цукру
+    model += 1 * A <= 30  # Обмеження для лимонного соку
+    model += 2 * B <= 40  # Обмеження для фруктового пюре
 
-# Розв'язання моделі
-model.solve()
+    # Розв'язання моделі
+    model.solve()
 
-# Вивід результатів
-print("Лимонад, од.:", A.varValue)  # 30 од.
-print("Фруктовий сік, од.:", B.varValue)  # 20 од.
+    # Вивід результатів
+    print("Статус:", pulp.LpStatus[model.status])  # Optimal
+    print("Лимонад, од.:", A.varValue)  # 30 од.
+    print("Фруктовий сік, од.:", B.varValue)  # 20 од.
+    print("Загальний обсяг, од.:", pulp.value(model.objective))  # 50 од.
+
+
+if __name__ == "__main__":
+    drinks_production()
